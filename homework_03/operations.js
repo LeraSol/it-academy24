f952a1d8e34f2908292edd60e5fb00ca7fbadb4b
Valeria Korenkova <lera.korenkova@list.ru>
2024-02-04 15:33:52 +0300

    operations with number, string, boolean

---
+++ homework_03/operations.js
@@ -0,0 +1,19 @@
+let str = 'abc';
+let number = 23;
+let a = true;
+
+console.log(str+a, str+number, number+a)
+
+console.log(str*a, str*number, number*a)
+
+console.log(str/a, str/number, number/a)
+
+console.log(parseInt("505"))
+console.log(typeof 505)
+let result;
+result = true.toString();
+console.log(result)
+console.log(typeof result)
+result = Boolean (number);
+console.log(result)
+console.log(typeof result)
